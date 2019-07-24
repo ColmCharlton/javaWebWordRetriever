600e199506cab359f00e740903059623458700f4
ColmCharlton <columcharlton@sidero.ie>
2019-07-24 18:09:55 +0100

    added resources

--- src/main/java/run.java
+++ src/main/java/run.java
@@ -18,8 +18,8 @@ public class run {
         final String wordsFinal;
 
 
-        String myURL = args[0];
-//        String myURL = "https://www.rte.ie/news/" ;
+//        String myURL = args[0];
+        String myURL = "https://www.rte.ie/news/" ;
 //        String myURL = "https://www.bbc.com/news" ;
 
 
@@ -36,13 +36,13 @@ public class run {
         //method call open file
         //Opening exclusionLists
         fileManagment fileManagment = new fileManagment();       //Create an instance of filereader
-//        String ExclusionList = fileManagment.fileIn("ExclusionList.txt");
-        String ExclusionList = fileManagment.fileIn("classes\\ExclusionList.txt");
+        String ExclusionList = fileManagment.fileIn("ExclusionList.txt");
+//        String ExclusionList = fileManagment.fileIn("classes\\ExclusionList.txt");
 
 
         fileManagment fileManagment2 = new fileManagment();         //Create an instance of filereader
-//        String ExclusionList_Keywords = fileManagment2.fileIn("ExclusionList_Keywords.txt");
-        String ExclusionList_Keywords = fileManagment2.fileIn("classes\\ExclusionList_Keywords.txt");
+        String ExclusionList_Keywords = fileManagment2.fileIn("ExclusionList_Keywords.txt");
+//        String ExclusionList_Keywords = fileManagment2.fileIn("classes\\ExclusionList_Keywords.txt");
 
 
 
