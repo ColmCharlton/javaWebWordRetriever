38d13625c3ff550854e675e1fdf4f02d1d70a335
ColmCharlton <columcharlton@sidero.ie>
2019-08-07 17:26:42 +0100

    Reverted to old files

---
+++ prepare_host.bat
@@ -0,0 +1,11 @@
+
+md C:\sonarPostgres\sonarqube\conf
+md C:\sonarPostgres\sonarqube\data
+md C:\sonarPostgres\sonarqube\extensions
+
+md C:\sonarPostgres\sonarqube\lib\bundled-plugins
+
+md C:\sonarPostgres\postgresql\data
+
+
+
