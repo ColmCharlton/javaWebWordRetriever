afbfb03861c5ee34cb981d0fe098ab4218b488d0
ColmCharlton <columcharlton@sidero.ie>
2019-09-10 09:34:01 +0100

    Testing jenkins backup

--- src/test/java/TestUrl.java
+++ src/test/java/TestUrl.java
@@ -6,13 +6,13 @@ import java.io.IOException;
 
 public class TestUrl {
 
-//    @Test
-//    public void httpRequest() throws IOException {
-////Check to see if a 200 response is given
-//        urlRetrieve url = new urlRetrieve();
-//        int urlS = url.urlCodeCheck("https://www.rte.ie/news/");
-//        Assert.assertThat(urlS, CoreMatchers.is(200));
-//    }
+    @Test
+    public void httpRequest() throws IOException {
+//Check to see if a 200 response is given
+        urlRetrieve url = new urlRetrieve();
+        int urlS = url.urlCodeCheck("https://www.rte.ie/news/");
+        Assert.assertThat(urlS, CoreMatchers.is(200));
+    }
 
     @Test
     public void httpReturnString() {
